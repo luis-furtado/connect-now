c71c2bd80c1304474c16b78b40b09fb4a1d6f512
Kevin Luis <k.luis360@gmail.com>
2023-12-17 18:40:06 -0300

    feat: adding video endpoint

--- server/api.py
+++ server/api.py
@@ -90,3 +90,24 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         users.append({"client_id":client_id,"status":"Status"})
         message = {"time":current_time,"clientId":client_id,"message":"Offline"}
         await manager.broadcast(json.dumps(message))
+
+@app.websocket("/ws/video/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    client={"client_id":client_id,"status":"Online"}
+    users.append(client)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            message = {"time":current_time,"clientId":client_id,"message":data}
+            await manager.broadcast(json.dumps(message))
+            
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        users.remove(client)
+        users.append({"client_id":client_id,"status":"Status"})
+        message = {"time":current_time,"clientId":client_id,"message":"Offline"}
+        await manager.broadcast(json.dumps(message))
